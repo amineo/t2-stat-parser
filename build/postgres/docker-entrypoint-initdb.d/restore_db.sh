15294ad2e501466f4e509a51ba81871c5f7a2a9d
Anthony Mineo <anthonymineo@gmail.com>
2020-03-08 14:26:17 -0400

    Use env user

--- build/postgres/docker-entrypoint-initdb.d/restore_db.sh
+++ build/postgres/docker-entrypoint-initdb.d/restore_db.sh
@@ -3,7 +3,7 @@
 echo "Setting up database..."
 BACKUP_FILE_PATH=/docker-entrypoint-initdb.d/backup/t2_stats.sql
 
-psql -d t2_stats -U dev -p 5432 -a -q -f $BACKUP_FILE_PATH
+psql -d t2_stats -U $POSTGRES_USER -p 5432 -a -q -f $BACKUP_FILE_PATH
 # -h PostgreSQL server IP address
 # -d database name
 # -U user name
