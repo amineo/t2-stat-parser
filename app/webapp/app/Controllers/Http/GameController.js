15b96d197e107b053518749f3ae785e62beb6e78
Anthony Mineo <anthonymineo@gmail.com>
2020-04-05 11:38:41 -0400

    init base game view/controller

--- app/webapp/app/Controllers/Http/GameController.js
+++ app/webapp/app/Controllers/Http/GameController.js
@@ -1,6 +1,30 @@
 'use strict'
 
+const Database = use('Database')
+
 class GameController {
+
+  async index({ inertia }) {
+
+    const pageTitle = "Last 1000 Games"
+    const gamesQry = await Database.table('games')
+      .distinct('game_id',
+                'map',
+                'stats',
+                'datestamp')
+      .where('game_id', '<>', 0)
+      .orderBy('game_id', 'desc')
+      .limit(1000)
+
+
+    // prep to combine objects for each game
+    let games = gamesQry
+
+
+
+    return inertia.render('Games/Main', { pageTitle, games }, { edgeVar: 'server-variable' })
+  }
+
 }
 
 module.exports = GameController
