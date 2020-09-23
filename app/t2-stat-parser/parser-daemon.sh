67eb8db0c821251659a12d45b2daa832cb7294c9
Anthony Mineo <anthonymineo@gmail.com>
2020-09-23 10:57:09 -0400

    parser update, non-restart - node cron

--- app/t2-stat-parser/parser-daemon.sh
+++ app/t2-stat-parser/parser-daemon.sh
@@ -5,5 +5,6 @@ echo "T2 Stat Parser Running...";
 # Run this initially, then we'll execute this on a schedule
 ./start.sh
 
+echo "T2 Stat Parser Done!";
 # Keep container running with the cron schedule
-crond -l 2 -f
+#crond -l 2 -f
